6dec3186b72c1d031f0a4ade43458ac78573ddcc
Priyadubal18 <priya.dubal18@gmail.com>
2023-03-12 15:39:35 -0400

    Refactor

--- web.py
+++ web.py
@@ -2,15 +2,17 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+
+
 def add_todo():
-    todo = st.session_state["new_todo"] +"\n"
+    todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity.")
-
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
