6d14dc0f46e6ead52a3d7a21ea34aaf9b4623ea6
Priyadubal18 <priya.dubal18@gmail.com>
2023-03-12 15:42:30 -0400

    test commit

--- web.py
+++ web.py
@@ -5,8 +5,8 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
+    todo_local = st.session_state["new_todo"] + "\n"
+    todos.append(todo_local)
     functions.write_todos(todos)
 
 
